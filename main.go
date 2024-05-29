9241d41c21b340fd41e3c871a9181d293dac5e71
LeeSW <tmddnr3163@gmail.com>
2024-05-30 00:01:12 +0900

    Go에서 외부 패키지 활용
    EXPORT는 대문자로

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/namlulu/learngo/something"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	something.SayHello()
+}
---
+++ something/something.go
@@ -0,0 +1,11 @@
+package something
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("Hello, World!")
+}
+
+func SayHello2() {
+	fmt.Println("Hello, World2!")
+}
