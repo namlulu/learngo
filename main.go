e37c96d09c0ef66ff50be6e92c1f23a40cdaa6cb
Namlulu <tmddnr3163@gmail.com>
2024-06-29 12:47:59 +0900

    1.3 functions

--- main.go
+++ main.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
+	"github.com/namlulu/learngo/something"
 )
 
+func mulitply(a int, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+
+}
+
 func main() {
+	// 1.1 Packages and Imports
+	something.SayHello()
+
+	// 1.2 Variables and Constants
 	const name string = "namlulu"
 	// name = "namlulu2"
 
@@ -14,4 +30,10 @@ func main() {
 	name3 := "namlulu3"
 
 	fmt.Println(name, name2, name3)
+
+	// 1.3 Functions part One
+	fmt.Println(mulitply(2, 2))
+	totalLength, upperName := lenAndUpper("namlulu")
+	fmt.Println(totalLength, upperName)
+
 }
